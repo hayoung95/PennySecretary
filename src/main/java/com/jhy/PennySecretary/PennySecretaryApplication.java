eb9b78ee58c673c987fcbe58d327c835a549df8d
DESKTOP-34I8073\JHY <ha016595@gmail.com>
2024-08-13 09:48:43 +0900

    프로잭트 재생성

---
+++ src/main/java/com/jhy/PennySecretary/PennySecretaryApplication.java
@@ -0,0 +1,13 @@
+package com.jhy.PennySecretary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennySecretaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennySecretaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhy/PennySecretary/PennySecretaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhy.PennySecretary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennySecretaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
